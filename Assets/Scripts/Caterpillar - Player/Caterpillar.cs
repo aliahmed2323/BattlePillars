d6cf5b5669ada225b61d9f08fbe9ee7eb5a5211c
Ali Ahmed <aaliahmed28@gmail.com>
2023-08-13 00:55:40 -0700

    Movement Added

---
+++ Assets/Scripts/Caterpillar - Player/Caterpillar.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Caterpillar : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _extensions = new();
+    [SerializeField] GameObject[] _extensionTypes;
+    [SerializeField] Button btn;
+    [SerializeField] float _caterPillarSpeed;
+
+    [HideInInspector]
+    public delegate void MoveAnim();
+    public event MoveAnim _moveAnim;
+
+    [HideInInspector]
+    public delegate void StopMoveAnim();
+    public event StopMoveAnim _stopMoveAnim;
+
+    private void Start()
+    {
+        _extensions.Add(transform.GetChild(0).gameObject);
+        btn.onClick.AddListener(() => AddExtension(0));
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        Vector2 newPos = Vector2.Lerp(transform.position, new Vector2(transform.position.x + 0.5f, transform.position.y), Time.deltaTime * _caterPillarSpeed);
+        transform.position = newPos;
+    }
+
+    public void AddExtension(int type)
+    {
+        Vector2 newPos = new Vector2(_extensions[_extensions.Count - 1].transform.position.x - 1.7f, transform.position.y);
+        GameObject ext = Instantiate(_extensionTypes[0], newPos, Quaternion.identity, transform);
+        _extensions.Add(ext);
+        if(_extensions.Count % 2 == 0)
+        ext.GetComponent<CaterpillarExtension>().animUp = true;
+    }
+
+    public void ReleaseCaterPillar()
+    {
+        _moveAnim.Invoke();
+    }
+}
---
+++ Assets/Scripts/Caterpillar - Player/CaterpillarExtension.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CaterpillarExtension : MonoBehaviour
+{
+    public bool animUp;
+
+    private void Start()
+    {
+        GetComponentInParent<Caterpillar>()._moveAnim += MoveAnim;
+    }
+
+    void MoveAnim()
+    {
+        if (animUp)
+            transform.DOLocalMoveY(0.7f, 0.4f).SetLoops(-1, LoopType.Yoyo);
+        else
+            transform.DOLocalMoveY(0.7f, 0.4f).SetDelay(0.4f).SetLoops(-1, LoopType.Yoyo);
+    }
+}
